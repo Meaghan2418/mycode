cfa6d758b65e283e1c7d5e267ac43448e027d7e8
Meaghan <Meaghan2418@users.noreply.github.com>
2022-10-15 19:57:50 +0000

    join and split

---
+++ challenge_one.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#Challenge One SHEBANG, INPUT, PRINT, CONCATENATE, VARIABLES
+
+def challenge():
+    name = user_input("Whoooo aaarrreee yyyyooouuu?")
+    dotwk = user_input("Where am I... wait don't answer that! What day of the week is this?")
+print("Well that didn't go as planned! Greetings," + name, "!")
+print("Happy" + dotwk, "!")
+
+challenge()
+
