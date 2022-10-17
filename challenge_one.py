ee345bbf31a46b260ce638dc66054f2d16c69bd4
Meaghan <Meaghan2418@users.noreply.github.com>
2022-10-17 17:57:26 +0000

    Challenge One corrections

--- challenge_one.py
+++ challenge_one.py
@@ -7,7 +7,7 @@ def challenge():
 
     dotwk = input("Ok, well good, good! What day of the week is this? ")
 
-    print(f"Well, {name} we've got to run! No time to chitchat! Just another {dotwk} in Gallifrey! Now, Run!")
+    print(f"Well, {name} we've got to go! No time to chitchat! Just another {dotwk} in Gallifrey! Now, Run!")
 
 challenge()
 
