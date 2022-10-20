0db2a15744120dba42c4413030c90cc7cd4931cb
Meaghan <Meaghan2418@users.noreply.github.com>
2022-10-20 20:05:27 +0000

    Lab 44 Challenge

---
+++ challenges/lab44.py
@@ -0,0 +1,7 @@
+"""M.Mahaffey Challenge Lab 44"""
+#!/usr/bin/env python3
+
+for bottles in range(1,100):
+    print(f"99 bottles of beer on the wall! {100-bottles} bottles of beer!")
+    print(f"Take one down, pass it around! {100-bottles} bottles of beer on the wall!")
+
